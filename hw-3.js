40e4a3aec4e2071e62ae80579f70d819521fd3b9
Antonova Anastasiya <alyunova@yandex.ru>
2025-12-11 21:23:54 +0500

    домашнее задание 3. задача 3

--- hw-3.js
+++ hw-3.js
@@ -6,3 +6,8 @@ password === tryPassword ? alert ('Пароль введен верно') : aler
 // Задача 2
 let c = 2;
 (c > 0) && (c < 10) ? console.log('Верно') : console.log('Неверно');
+
+// Задача 3
+let d = 0;
+let e = 500;
+(d > 100) || (e > 100) ? console.log("Верно") : console.log("Неверно");
