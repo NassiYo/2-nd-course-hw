14b61a3d21a6dd9990f98fba5cda86b3b5f99d0e
Antonova Anastasiya <alyunova@yandex.ru>
2025-12-12 08:30:00 +0500

    домашнее задание 3. Задача 5

--- hw-3.js
+++ hw-3.js
@@ -16,3 +16,31 @@ let e = 500;
 let a = '2';
 let b = '3';
 alert(Number(a) + Number(b));
+
+// Задача 5
+monthNumber = 12;
+switch (monthNumber) {
+    case 12:
+    case 1:
+    case 2:
+        console.log("Зима");
+        break;
+    case 3:
+    case 4:
+    case 5:
+        console.log("Весна");
+        break;
+    case 6:
+    case 7:
+    case 8:
+        console.log("Лето");
+        break;
+    case 9:
+    case 10:
+    case 11:
+        console.log("Осень");
+        break;
+    default:
+        console.log("Нет такого месяца");
+        break;
+}
