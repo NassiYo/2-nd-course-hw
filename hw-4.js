3c5c15be74995dd9aa49db68946e37a0fce6f2f2
Antonova Anastasiya <alyunova@yandex.ru>
2025-12-16 17:38:15 +0500

    дз4. задача 3

--- hw-4.js
+++ hw-4.js
@@ -1,9 +1,14 @@
-// Задача 1
-for (let i = 0; i < 2; i++) {
-  console.log('Привет');
-}
+// // Задача 1
+// for (let i = 0; i < 2; i++) {
+//   console.log('Привет');
+// }
 
-// Задача 2
-for (let i = 0; i < 5; i++) {
-  console.log(i + 1);
+// // Задача 2
+// for (let i = 0; i < 5; i++) {
+//   console.log(i + 1);
+// }
+
+// Задача 3
+for (let i = 7; i < 23; i++) {
+  console.log(i);
 }
