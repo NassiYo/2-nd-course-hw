a815bb3c092cbc716db375f3e19a6fda6ba0cff2
Antonova Anastasiya <alyunova@yandex.ru>
2025-12-16 17:45:45 +0500

    дз4. задача 4

--- hw-4.js
+++ hw-4.js
@@ -8,7 +8,17 @@
 //   console.log(i + 1);
 // }
 
-// Задача 3
-for (let i = 7; i < 23; i++) {
-  console.log(i);
+// // Задача 3
+// for (let i = 7; i < 23; i++) {
+//   console.log(i);
+// }
+
+const obj = {
+    "Коля" : '200',
+    "Вася" : '300',
+    "Петя" : '400'
+};
+
+for (const key in obj) {
+  console.log(`${key} — зарплата ${obj[key]} долларов`);
 }
