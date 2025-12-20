f043d614eb50df431e928ffd5b7731d6c94e7c44
Antonova Anastasiya <alyunova@yandex.ru>
2025-12-20 19:14:12 +0500

    Домашнее задание 5. Сделана Игра 1 Угадай число

---
+++ hw-5.js
@@ -0,0 +1,22 @@
+// Игра "Угадай число"
+function startGame1() {
+    const randomNumber = Math.floor(Math.random() * 100) + 1;
+
+    while (true) {
+        let userNumber = prompt('Угадай число от 1 до 100');
+        userNumber = Number(userNumber);
+
+        if (Number.isNaN(userNumber)) {
+            alert("Вы ввели не число");
+            continue;
+        } else if (userNumber === randomNumber) {
+            alert("Ура, ты угадал :)");
+            break;
+        } else if (userNumber > randomNumber) {
+            alert("Неверно, загаданное число меньше. Попробуй еще раз.")
+        } else {
+            alert("Неверно, загаданное число больше. Попробуй еще раз.")
+        }
+    }
+}
+
