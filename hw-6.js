d57fa94d65023847364a56205a9f5f99c2819287
Antonova Anastasiya <alyunova@yandex.ru>
2025-12-30 17:50:25 +0500

    ДЗ 6. Задача 1

--- hw-6.js
+++ hw-6.js
@@ -1,14 +1,23 @@
+// Задача 1
+const arr1 = [1, 5, 4, 10, 0, 3];
+for (let i = 0; i < arr1.length; i++) {
+    console.log(arr1[i]);
+    if (arr1[i] === 10) {
+        break;
+    }
+};
+
 // // Задача 9
 // const arr9 = [[1, 2, 3],[4, 5, 6]];
 // console.log([...arr9[0], ...arr9[1]]);
 
-// Задача 10
-let arr10 = [];
+// // Задача 10
+// let arr10 = [];
 
-for (let i = 0; i < 10; i++) {
-    arr10.push(Math.floor(Math.random() * 10) + 1);
-};
-console.log(arr10);
-for (let i = 0; i < 9; i++) {
-    console.log(arr10[i]+arr10[i + 1])
-};
+// for (let i = 0; i < 10; i++) {
+//     arr10.push(Math.floor(Math.random() * 10) + 1);
+// };
+// console.log(arr10);
+// for (let i = 0; i < 9; i++) {
+//     console.log(arr10[i]+arr10[i + 1])
+// };
