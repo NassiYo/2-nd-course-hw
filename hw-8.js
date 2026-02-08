a43dcaf401c40f99ecfaa0952f5ad50955c4bfbd
Antonova Anastasiya <alyunova@yandex.ru>
2026-02-08 19:07:00 +0500

    дз8. Задача 5

--- hw-8.js
+++ hw-8.js
@@ -74,14 +74,34 @@ function startGame4() {
 // }, 30000);
 
 
-// Задача 4
-function delayForSecond(callback) {
-    // Код писать можно только внутри этой функции
-    setTimeout(() => callback(), 1000);
+// // Задача 4
+// function delayForSecond(callback) {
+//     // Код писать можно только внутри этой функции
+//     setTimeout(() => callback(), 1000);
     
-    // callback();
+//     // callback();
+// }
+
+// delayForSecond(function () {
+//    console.log('Привет, Глеб!');
+// });
+
+// Задача 5
+// Функция delayForSecond через 1 секунду пишет в консоль 
+// «Прошла одна секунда», а затем вызывает переданный колбэк
+function delayForSecond(cb) {
+    setTimeout(() => {
+        console.log('Прошла одна секунда');
+        if(cb) {  cb(); }
+    }, 1000)
 }
 
-delayForSecond(function () {
-   console.log('Привет, Глеб!');
-})
+// Функция sayHi выводит в консоль приветствие для указанного имени
+function sayHi (name) {
+    console.log(`Привет, ${name}!`);
+}
+
+// Код выше менять нельзя
+
+// Нужно изменить код ниже:
+delayForSecond(() => sayHi('Глеб'));
