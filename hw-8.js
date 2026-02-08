08457114cfde2a5c0944210dfb4a6d426e3112cf
Antonova Anastasiya <alyunova@yandex.ru>
2026-02-08 18:17:39 +0500

    дз8. реализована игра камень ножницы бумага

--- hw-8.js
+++ hw-8.js
@@ -1,3 +1,34 @@
+// Игра "Камень, ножницы, бумага"
+
+function startGame4() {
+    const choices = ['камень', 'ножницы', 'бумага']; //массив возможных выриантов
+    let userChoice = prompt("Выбери: камень, ножницы или бумага?").toLowerCase(); //выбор пользователя сводится к нижнему регистру
+    let compChoice = choices[Math.floor(Math.random() * 3)]; // выбор компьютера. Из массива возможных вариантов выбирается выриант по случайному индексу. Индекс - случайное число от 0 до 3, не включая 3. 
+
+    if (choices.includes(userChoice)) {
+        alert(`Выбор пользователя - ${userChoice}, выбор компьютера - ${compChoice}.`); //Если игрок ввел выбор верно, то выводится выбор игрока и выбор компьютера
+    } else {
+        alert('Выбор не сделан или введен неверно.'); //Если игрок ввел значение с ошибкой, то компьютер об этом сообщает и игрок проигрывает.
+    };
+
+    let result = '';
+
+    if (userChoice === compChoice) {
+        result = 'ничья';
+    } else if ((userChoice === 'камень' && compChoice === 'ножницы') || (userChoice === 'ножницы' && compChoice === 'бумага') || (userChoice === 'бумага' && compChoice === 'камень')) {
+        result = 'победа';
+    } else {
+        result = 'проигрыш';
+    };
+    alert(`Результат - ${result}.`);
+}
+
+
+
+
+
+
+
 // // Задача 1
 // const people = [
 //    { name: 'Глеб', age: 29 },
@@ -8,31 +39,31 @@
 
 // console.log(people.sort((a, b) => a.age - b.age));
 
-// Задача 2
-function isPositive(a) {
-    return a > 0;
-}
+// // Задача 2
+// function isPositive(a) {
+//     return a > 0;
+// }
 
-function isMale(a) {
-    return a.gender === 'male';
-}
+// function isMale(a) {
+//     return a.gender === ' male';
+// }
 
-function filter(arr, ruleFunction) {
-    const output = [];
-    for (let i = 0; i < arr.length; i++) {
-        (ruleFunction(arr[i])) && output.push(arr[i]);
-    };
+// function filter(arr, ruleFunction) {
+//     const output = [];
+//     for (let i = 0; i < arr.length; i++) {
+//         (ruleFunction(arr[i])) && output.push(arr[i]);
+//     };
 
-    return output;
-}
+//     return output;
+// }
 
-console.log(filter([3, -4, 1, 9], isPositive));
+// console.log(filter([3, -4, 1, 9], isPositive));
 
-const people1 = [
-   {name: 'Глеб', gender: 'male'},
-   {name: 'Анна', gender: 'female'},
-   {name: 'Олег', gender: 'male'},
-   {name: 'Оксана', gender: 'female'}
-];
+// const people1 = [
+//    {name: 'Глеб', gender: 'male'},
+//    {name: 'Анна', gender: 'female'},
+//    {name: 'Олег', gender: 'male'},
+//    {name: 'Оксана', gender: 'female'}
+// ];
 
-console.log(filter(people1, isMale));
+// console.log(filter(people1, isMale));
