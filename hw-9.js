a1d00c37a6be8af11c2e23afdfcbd05d23e2133b
Antonova Anastasiya <alyunova@yandex.ru>
2026-02-18 20:53:35 +0500

    дз9 Задача 2

--- hw-9.js
+++ hw-9.js
@@ -1,8 +1,15 @@
 // Задача 1
-
 const btnEl = document.querySelector('.btn');
 
 btnEl.addEventListener('click', () => {
     const titleEl = document.querySelector('.title');
     titleEl.classList.toggle('title_hidden');
+});
+
+// Задача 2
+const btnChangeColorEl = document.querySelector('.btn-change-color');
+
+btnChangeColorEl.addEventListener('click', () => {
+    const textEl = document.querySelector('.text');
+    textEl.style.color = 'blue';
 });
