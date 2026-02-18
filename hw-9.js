2e81773572cbe88df66a85058d6330d77b8ebe38
Antonova Anastasiya <alyunova@yandex.ru>
2026-02-18 20:47:49 +0500

    дз9 создан отдельный файл hw9.html, выполнена Задача 1

--- hw-9.js
+++ hw-9.js
@@ -1,1 +1,8 @@
 // Задача 1
+
+const btnEl = document.querySelector('.btn');
+
+btnEl.addEventListener('click', () => {
+    const titleEl = document.querySelector('.title');
+    titleEl.classList.toggle('title_hidden');
+});
